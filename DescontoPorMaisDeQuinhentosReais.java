68319495af8f33805e84292a3533b7c4f5d2daa2
Matheus C O Santos <oliveiramatheus77@gmail.com>
2020-06-04 15:41:57 -0300

    feat: Chain of Responsability

---
+++ DescontoPorMaisDeQuinhentosReais.java
@@ -0,0 +1,19 @@
+public class DescontoPorMaisDeQuinhentosItens implements Desconto{
+    private Desconto proximo;
+
+    public double desconta(Orcamento orcamento){
+        if(orcamento.getItens().size() > 500){
+            return orcamento.getValor() * 0.07;
+        }else{
+            return proximo.desconta(orcamento);
+        }
+    }
+
+
+
+    @Override
+    public void setProximo(Desconto proximo) {
+
+        this.proximo = proximo;
+    }
+}
