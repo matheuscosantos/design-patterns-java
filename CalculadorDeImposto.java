0abee3e401a9279d41bb71c66f1b9baf8f6d0941
Matheus C O Santos <oliveiramatheus77@gmail.com>
2020-06-04 15:02:20 -0300

    feat: strategy

---
+++ CalculadorDeImposto.java
@@ -0,0 +1,8 @@
+public class CalculadorDeImposto {
+
+    public void realizaCalculo(Orcamento orcamento, Imposto impostoQualquer) {
+        double icms = impostoQualquer.calcula(orcamento);
+        System.out.println(icms);
+    }
+
+}
---
+++ TesteDeImpostos.java
@@ -0,0 +1,15 @@
+public class TesteDeImpostos {
+
+    public static void main(String[] args) {
+        Imposto iss = new ISS();
+        Imposto icms = new ICMS();
+
+        Orcamento orcamento = new Orcamento(500.0);
+
+        CalculadorDeImposto calculadorDeImposto = new CalculadorDeImposto();
+
+        calculadorDeImposto.realizaCalculo(orcamento, iss);
+        calculadorDeImposto.realizaCalculo(orcamento, icms);
+
+    }
+}
